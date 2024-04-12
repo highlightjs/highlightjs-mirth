cde723128c7b8ecb8226da0f2bb10d76032e61f6
casuallyblue <sierra@casuallyblue.dev>
2024-04-12 17:23:08 -0500

    Add hilightjs

--- src/languages/mirth.js
+++ src/languages/mirth.js
@@ -1,5 +1,5 @@
 /*
-Language: C
+Language: mirth
 Category: common
 Website: https://github.com/mirth-lang/mirth
 */
@@ -9,23 +9,24 @@ export default function(hljs) {
   const regex = hljs.regex;
 	
 	const MIRTH_LINE_COMMENT_MODE = hljs.COMMENT('#', '$');
-	const MIRTH_DOC_COMMENT_MODE = hljs.COMMENT('\|\|\|', '$');
+	const MIRTH_DOC_COMMENT_MODE = hljs.COMMENT('\\|\\|\\|', '$');
 
 	const NUMBERS = {
 		className: 'number',
 		variants: [
-			{ begin: '([+-]?)\\b([0-9]+)' },
-			{ begin: '([+-]?)(\\b0[xX][a-fA-F0-9]+)' }
+			{ begin: /[+-]?([0-9]+)/ },
+			{ begin: /[+-]?(0[xX][a-fA-F0-9]+)/ }
 		],
 		relevance: 0
 	};
 
 	const WORD = {
-		className: 'entity.name.word.mirth',
+		className: 'identifier',
 		variants: [
-			{ begin: '[\-a-z0-9_]+' },
+			{ begin: /[\-a-z0-9_\!\?\/]+/, end: /( |\()/ },
 		],
 
+		illegal: [ "keyword" ],
 	};
 
 	const KEYWORDS = [
@@ -38,38 +39,99 @@ export default function(hljs) {
 
 		"data",
 		"table",
-		"field",
-		
+		"field",	
 	];
 
 	const TYPE = {
-		className: 'entity.name.type.mirth',
+		className: 'type',
 		variants: [
-			{ begin: '[a-z]+(\*?)' },
-			{ begin: '[A-Z][a-zA-Z0-9_\-]*' },
+			{ begin: /\*?([a-z]+)/ },
+			{ begin: /[A-Z]([a-zA-Z0-9_\-]*)/ },
+			{ begin: /\+[A-Z]([a-zA-Z0-9_\-]*)/ },
+		],
+
+		contains: [
+			{ begin: '\\(', end: '\\)', contains: [ "self" ] }
+		],
+	};
+
+	const MODULE = {
+		className: 'module',
+		begin: ['module', '\\(' ],
+		beginScope: {1: "keyword" },
+		end: '\\)',
+
+		contains: [{ begin: '[a-zA-Z\.\-]' }],
+
+		illegal: [ "keyword" ]
+	};
+
+	const IMPORT = {
+		className: 'module',
+		begin: ['import', '\\(' ],
+		beginScope: {1: "keyword" },
+		end: '\\)',
+
+		contains: [{ begin: '[a-zA-Z\.\-]' }],
+
+		illegal: [ "keyword" ]
+	};	
+
+	const TYPE_DEFINITION = {
+		className: 'type',
+		begin: ['def-type', '\\(' ],
+		beginScope: {1: "keyword" },
+		end: '\\)',
+
+		contains: [
+			TYPE
 		]
 	};
 
 	const DEFINITION = {
-		className: 'entity.name.function.mirth',
-		begin: "def\(",
-		end: "\)",
+		className: 'function',
+		begin: ['def', '\\('],
+		beginScope: { 1: "keyword" },
+		end: '\\)',
 
 		contains: [
-			TYPE,
+			hljs.QUOTE_STRING_MODE,
+			{ begin: /\-\-/ },
 			WORD,	
+			TYPE,
 			NUMBERS,
 		]
 	};
+
+	const EXTERN_DEFINITION = {
+		className: 'function',
+		begin: ['def-extern', '\\('],
+		beginScope: { 1: "keyword" },
+		end: '\\)',
+
+		contains: [
+			{ begin: /\-\-/ },
+			WORD,	
+			TYPE,
+		]
+	}
 	
 	return {
 		name: "mirth",
 		aliases: ['mth'],
-		keywords: KEYWORDS,
+		keywords: { 
+		        keyword: KEYWORDS,
+			literal: ['F', 'T']
+		},
 		contains: [].concat(
+			hljs.QUOTE_STRING_MODE,
 			MIRTH_LINE_COMMENT_MODE,
 			MIRTH_DOC_COMMENT_MODE,
-			DEFINITION	
+			DEFINITION,
+			TYPE_DEFINITION,
+			EXTERN_DEFINITION,
+			IMPORT,
+			MODULE
 		),
 
 		exports: {
